6566e89c0d7f67361df6afb0188a867291c84782
David Medina <david.medina@gamechanger.io>
2019-03-31 23:23:02 -0400

    no message

---
+++ app/src/main/java/inc/grayherring/com/thedavidmedinashowapp/data/models/NASAPlanetary.kt
@@ -0,0 +1,13 @@
+package inc.grayherring.com.thedavidmedinashowapp.data.models
+
+import com.squareup.moshi.Json
+
+data class NasaPlanetary (
+	val date : String,
+	val explanation : String,
+	val hdurl : String,
+	@field:Json(name = "media_type") val mediaType : String, //todo change to enum video / image
+	@field:Json(name = "service_version")	val serviceVersion : String,
+	val title : String,
+	val url : String
+)
